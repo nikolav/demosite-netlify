abb80464ac91f6836327c5230c573999a44c199b
nikolav <vukovicnikola2014@gmail.com>
2021-07-07 09:39:22 +0100

    adds .php

---
+++ README.md
@@ -0,0 +1,3 @@
+admin.name=nikolav
+admin.url=nikolav.rs
+upload-dir-at-nikolav.rs=app/01
--- app.php
+++ app.php
@@ -1,3 +1,9 @@
 <?php
 
-echo date();
+$data = "data.1=". mt_rand();
+
+header("Content-Type: text/plain");
+echo $data;
+
+exit;
+//eof
